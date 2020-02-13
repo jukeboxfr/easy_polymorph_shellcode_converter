25eca4d4a86b34e356677c2439907b4fb04bc55f
Keny SAINT HILAIRE <kesaint-@e1r3p13.42.fr>
2020-02-13 16:53:19 +0100

    Removed tests directory

--- tests/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/usr/bin/nasm shellcode.asm
-xxd -i shellcode | sed 1d | sed '$d' | sed '$d' | xargs echo -n | tr -d '\n' | sed '1s/^/[/' | sed "$ s/$/]\n/"
--- tests/test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-xxd -i lol | sed 1d | sed '$d' | sed '$d' | xargs echo -n | tr -d '\n' | sed '1s/^/[/' | sed "$ s/$/]\n/"
